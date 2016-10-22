a84222fec45f329ba1f3d53070bc59e7a6eaa7cc
YOUSSOUF ISMAIL CHERIFI <youcef.ismail.cherifi@gmail.com>
2016-10-22 16:05:53 +0100

    Add files via upload

---
+++ Scripts/SpawnMonuments.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnMonuments : MonoBehaviour {
+    public GameObject[] monuments;
+    public float secondsBetweenSpawns;
+
+    int count;
+	// Use this for initialization
+	void Start () {
+        count = 0;
+        Spawn();
+	}
+
+    void Spawn()
+    {
+        Instantiate(monuments[count], transform.position, transform.rotation);
+        count++;
+        if (count < monuments.Length)
+            Invoke("Spawn", secondsBetweenSpawns);
+    }
+}
